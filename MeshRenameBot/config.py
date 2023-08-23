f5d540795fbb391baa635e32665de5a18549dac1
PyroUserBot <129763781+PyroUserBot@users.noreply.github.com>
2023-08-23 22:35:17 +0530

    Update config.py

--- MeshRenameBot/config.py
+++ MeshRenameBot/config.py
@@ -4,19 +4,19 @@ try:
     from .tconfig import Config
 except ImportError:
     class Config:
-        DATABASE_URL = [str, ""]
-        API_HASH = [str, "abcdedf......"]
-        API_ID = [int, 1234567]
-        BOT_TOKEN = [str, "bot:token here"]
+        DATABASE_URL = [str, "mongodb+srv://sonu55:sonu55@cluster0.vqztrvk.mongodb.net/?retryWrites=true&w=majority"]
+        API_HASH = [str, "2b445de78e5baf012a0793e60bd4fbf5"]
+        API_ID = [int, 19099900]
+        BOT_TOKEN = [str, "6146904519:AAFtE6fUIDoM9xOKZqmrbs2gA5-2FGdDGh0"]
         COMPLETED_STR = [str, "▰"]
         REMAINING_STR = [str, "▱"]
         MAX_QUEUE_SIZE = [int, 5]
         SLEEP_SECS = [int, 10]
-        IS_MONGO = [bool, False]
+        IS_MONGO = [bool, True]
 
         # Access Restriction
-        IS_PRIVATE = [bool, False]
-        AUTH_USERS = [list,[123456789]]
+        IS_PRIVATE = [bool, True]
+        AUTH_USERS = [list,[778393824 6265459491 6198858059]]
         OWNER_ID = [int, 0]
 
         # Public username url or invite link of private chat
