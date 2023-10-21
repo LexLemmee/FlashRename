e861b880bd4b28c81bc853ec413610d6b2b7df64
Kunal Gaikwad <129763781+AnimeCampus@users.noreply.github.com>
2023-10-21 10:21:20 +0530

    Update Executor.py

--- MeshRenameBot/maneuvers/Executor.py
+++ MeshRenameBot/maneuvers/Executor.py
@@ -4,42 +4,37 @@ from .Default import DefaultManeuver
 
 renamelog = logging.getLogger(__name__)
 
-
-class Executor():
+class Executor:
     def __init__(self, queue: asyncio.Queue, workerid: int) -> None:
         self.maneuvers_queue = queue
-        renamelog.info("Started executor with id {workerid} successfully".format(workerid=workerid))
-        self._stop = False
         self.workerid = workerid
+        self._stop = False
         self._current_maneuver = None
-
+        renamelog.info("Started executor with id {workerid} successfully".format(workerid=workerid))
         asyncio.get_event_loop().create_task(self.execute())
 
     async def execute(self) -> None:
-        while True:
-            if self._stop:
-                break
-            
+        while not self._stop:
             maneuver = await self.maneuvers_queue.get()
 
             if maneuver.is_canceled:
-                ...
+                # Handle canceled maneuver
+                continue
             elif maneuver.is_halted:
+                # Requeue halted maneuver
                 await self.maneuvers_queue.put(maneuver)
             else:
                 self._current_maneuver = maneuver
                 try:
                     await maneuver.execute()
                     maneuver.done()
-                except:
+                except Exception as e:
                     maneuver.done()
-                    renamelog.exception("Execute failed")
+                    renamelog.exception("Execute failed: %s", str(e))
                 self._current_maneuver = None
 
             await asyncio.sleep(3)
-        
-        # renamelog.debug("Started stopped with id {workerid} successfully".format(workerid=self.workerid))
-    
+
     def stop(self) -> None:
         if self._current_maneuver is not None:
             self._current_maneuver.cancel()
