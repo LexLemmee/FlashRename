ead18ece1d016307ebad1d45759cb0e068359dab
Kunal Gaikwad <129763781+AnimeCampus@users.noreply.github.com>
2023-10-21 10:28:24 +0530

    Update mode_select.py

--- MeshRenameBot/core/mode_select.py
+++ MeshRenameBot/core/mode_select.py
@@ -1,45 +1,41 @@
-from pyrogram.types.bots_and_keyboards.inline_keyboard_button import InlineKeyboardButton
-from pyrogram.types.bots_and_keyboards.inline_keyboard_markup import InlineKeyboardMarkup
-from pyrogram.types.messages_and_media.message_entity import MessageEntity
-from pyrogram.types.user_and_chats.user import User
-from ..database.user_db import UserDB
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, MessageEntity
 from pyrogram import Client
+from ..database.user_db import UserDB
 from pyrogram.types import Message
 
-#TODO trans pending
+# TODO: Add translations for messages
 
-async def upload_mode(client: Client, msg: Message ) -> None:
+async def upload_mode(client: Client, msg: Message) -> None:
     user_id = msg.from_user.id
     udb = UserDB()
     modee = udb.get_mode(user_id)
-    
-    msg_str =""
+
+    msg_str = ""
 
     if modee == UserDB.MODE_SAME_AS_SENT:
         msg_str += "➡️ "
-    
-    msg_str += "1. MODE to the same format as it was sent.\n"    
+
+    msg_str += "1. MODE to the same format as it was sent.\n"
     if modee == UserDB.MODE_AS_DOCUMENT:
         msg_str += "➡️ "
-    
+
     msg_str += "2. MODE Forced Document.\n"
-    
+
     if modee == UserDB.MODE_AS_GMEDIA:
         msg_str += "➡️ "
-    
+
     msg_str += "3. Mode General Media."
 
     markup = InlineKeyboardMarkup(
         [
             [
-                InlineKeyboardButton("1","mode set 0"),
-                InlineKeyboardButton("2","mode set 1"),
-                InlineKeyboardButton("3","mode set 2"),
+                InlineKeyboardButton("1", "mode set 0"),
+                InlineKeyboardButton("2", "mode set 1"),
+                InlineKeyboardButton("3", "mode set 2"),
             ]
         ]
     )
 
-    
     await msg.reply_text(msg_str, quote=True, reply_markup=markup)
 
 async def mode_callback(client, msg: MessageEntity):
@@ -47,37 +43,37 @@ async def mode_callback(client, msg: MessageEntity):
     smode = int(data[-1])
     udb = UserDB()
     user_id = msg.message.reply_to_message.from_user.id
-    
-    udb.set_mode(smode, user_id)
-    await msg.answer("Mode changes successfully.")
 
-    msg_str =""
+    udb.set_mode(smode, user_id)
+    await msg.answer("Mode changed successfully.")
+
+    msg_str = ""
 
     if smode == UserDB.MODE_SAME_AS_SENT:
         msg_str += "➡️ "
-    
-    msg_str += "1. MODE to the same format as it was sent.\n"    
+
+    msg_str += "1. MODE to the same format as it was sent.\n"
     if smode == UserDB.MODE_AS_DOCUMENT:
         msg_str += "➡️ "
-    
+
     msg_str += "2. MODE Forced Document.\n"
-    
+
     if smode == UserDB.MODE_AS_GMEDIA:
         msg_str += "➡️ "
-    
+
     msg_str += "3. Mode General Media."
 
     markup = InlineKeyboardMarkup(
         [
             [
-                InlineKeyboardButton("1","mode set 0"),
-                InlineKeyboardButton("2","mode set 1"),
-                InlineKeyboardButton("3","mode set 2"),
+                InlineKeyboardButton("1", "mode set 0"),
+                InlineKeyboardButton("2", "mode set 1"),
+                InlineKeyboardButton("3", "mode set 2"),
             ]
         ]
     )
 
     try:
-        await msg.message.edit_text(msg_str, reply_markup = markup)
+        await msg.message.edit_text(msg_str, reply_markup=markup)
     except:
         pass
