59411f83296bacc1e7529435e1cd1fd742434140
LexLemmee <157198289+LexLemmee@users.noreply.github.com>
2024-02-01 22:06:11 +0530

    Update config.py

--- MeshRenameBot/config.py
+++ MeshRenameBot/config.py
@@ -4,23 +4,23 @@ try:
     from .tconfig import Config
 except ImportError:
     class Config:
-        DATABASE_URL = [str, "postgres://iubxmjfs:XtftA36D6kajzosJinXlepPr40xId6Df@trumpet.db.elephantsql.com/iubxmjfs"]
-        API_HASH = [str, "2b445de78e5baf012a0793e60bd4fbf5"]
-        API_ID = [int, 19099900]
-        BOT_TOKEN = [str, "6295293651:AAEMIVNFEgMwiuzPEoWLRU9arF9kbPzOrA0"]
+        DATABASE_URL = [str, "mongodb+srv://sturdex:123@cluster0.cusrjyp.mongodb.net/?retryWrites=true&w=majority"]
+        API_HASH = [str, "10e3ed833b0d09699973420d45359409"]
+        API_ID = [int, 4665778]
+        BOT_TOKEN = [str, "6067088569:AAFuJk7fdGfDceeqSHNf964d30hKiwNstoI"]
         COMPLETED_STR = [str, "▰"]
         REMAINING_STR = [str, "▱"]
         MAX_QUEUE_SIZE = [int, 5]
         SLEEP_SECS = [int, 10]
-        IS_MONGO = [bool, False]
+        IS_MONGO = [bool, True]
 
         # Access Restriction
         IS_PRIVATE = [bool, False]
-        AUTH_USERS = [list,[6295293651]]
-        OWNER_ID = [int, 0]
+        AUTH_USERS = [list,[5531584953]]
+        OWNER_ID = [int, 5531584953]
 
         # Public username url or invite link of private chat
-        FORCEJOIN = [str,"https://t.me/illuminatiXNetwork"]
+        FORCEJOIN = [str,"https://t.me/Aogiri_Network"]
         FORCEJOIN_ID = [int,-1002058050288]
 
         TRACE_CHANNEL = [int, -1002058050288]
